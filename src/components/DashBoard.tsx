ef1ccf90e9d318aa3ced79da6a7d941b438fe617
cauatn <tavarescauac@gmail.com>
2024-07-25 21:41:55 -0300

    refactor: Update Dashboard component, filterChartData function, and ChartData component

--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -1,5 +1,5 @@
 import { TrendingUp } from "lucide-react";
-import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
 import {
   Card,
   CardContent,
@@ -27,6 +27,16 @@ const chartData = data.map((item: any) => {
   };
 });
 
+function getChartDataStatus(
+  chartData: { date: string; time: number }[]
+): { color: string }[] {
+  return chartData.map((item) => {
+    return {
+      color: item.time ? "bg-emerald-600" : "bg-red-600",
+    };
+  });
+}
+
 const chartConfig = {
   views: {
     label: "tempo (segundos)",
@@ -38,91 +48,117 @@ const chartConfig = {
 } satisfies ChartConfig;
 
 function Dashboard() {
-  const [timeRange] = useState("90d");
+  const [timeRange] = useState(90);
 
-  const filteredData = chartData.filter((item: any) => {
-    const date = new Date(item.date);
-    const now = new Date();
+  function filterChartData(chartData: any[], timeRange: number): any[] {
+    return chartData.filter((item: any) => {
+      const date = new Date(item.date);
+      const now = new Date();
 
-    let daysToSubtract = 90;
-    if (timeRange === "30d") {
-      daysToSubtract = 30;
-    } else if (timeRange === "7d") {
-      daysToSubtract = 7;
-    }
-    now.setDate(now.getDate() - daysToSubtract);
-    return date >= now;
-  });
+      let daysToSubtract = 90;
+
+      daysToSubtract = timeRange;
+
+      now.setDate(now.getDate() - daysToSubtract);
+      return date >= now;
+    });
+  }
 
   return (
-    <main className="h-full w-full py-2 px-4 flex justify-start flex-col space-y-8">
-      <Card className="w-full h-fit">
-        <CardHeader>
-          <CardTitle>Gráfico de tempo</CardTitle>
-          <CardDescription>
-            Gráfico para você ter um controle maior sobre o tempo que você está
-            se esforçando :)
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <ChartContainer
-            config={chartConfig}
-            className="aspect-auto h-[250px] w-full"
-          >
-            <BarChart
-              accessibilityLayer
-              data={filteredData}
-              margin={{
-                left: 12,
-                right: 12,
-              }}
-            >
-              <CartesianGrid vertical={false} />
-              <XAxis
-                dataKey="date"
-                tickLine={false}
-                axisLine={false}
-                tickMargin={8}
-                minTickGap={32}
-                tickFormatter={(value) => {
-                  const date = new Date(value);
-                  return date.toLocaleDateString("en-US", {
-                    month: "short",
-                    day: "numeric",
-                  });
-                }}
-              />
-              <ChartTooltip
-                content={
-                  <ChartTooltipContent
-                    className="w-[150px]"
-                    nameKey="views"
-                    labelFormatter={(value) => {
-                      return new Date(value).toLocaleDateString("en-US", {
-                        month: "short",
-                        day: "numeric",
-                        year: "numeric",
-                      });
-                    }}
-                  />
-                }
-              />
-              <Bar dataKey={"time"} fill={"hsl(var(--chart-2))"} />
-            </BarChart>
-          </ChartContainer>
-        </CardContent>
-        <CardFooter className="h-fit max-h-10">
-          <div className="flex w-full items-start gap-2 text-sm">
-            <div className="grid gap-2">
-              <div className="flex items-center gap-2 font-medium leading-none">
-                Historico recente <TrendingUp className="h-4 w-4" />
-              </div>
+    <main className="py-2 px-4 flex justify-start flex-col space-y-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <Card className="w-full h-fit">
+          <CardHeader>
+            <CardTitle>Tempo total na semana</CardTitle>
+            <CardDescription>Horas feitas durante a semana</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="flex items-center justify-center space-x-2">
+              <span className="text-3xl font-bold">
+                {filterChartData(data, 7).reduce((acc: number, item: any) => {
+                  return Number((acc + item.time / 3600).toFixed(2));
+                }, 0)}
+              </span>
+              <span className="text-lg">horas</span>
             </div>
-          </div>
-        </CardFooter>
-      </Card>
+          </CardContent>
+        </Card>
+      </div>
+      <ChartData data={filterChartData(chartData, timeRange)} />
       <SessionsList />
     </main>
+  );
+}
+
+function ChartData({ data = [] }: { data: { date: string; time: number }[] }) {
+  return (
+    <Card className="w-full h-fit">
+      <CardHeader>
+        <CardTitle>Gráfico de tempo</CardTitle>
+        <CardDescription>
+          Gráfico para você ter um controle maior sobre o tempo que você está se
+          esforçando :)
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer
+          config={chartConfig}
+          className="aspect-auto h-[250px] w-full"
+        >
+          <BarChart
+            accessibilityLayer
+            data={data}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <YAxis domain={[0, "dataMax"]} />
+            <XAxis
+              dataKey="date"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              minTickGap={32}
+              tickFormatter={(value) => {
+                const date = new Date(value);
+                date.setDate(date.getDate() + 1);
+                return date.toLocaleDateString("en-US", {
+                  month: "short",
+                  day: "numeric",
+                });
+              }}
+            />
+            <ChartTooltip
+              content={
+                <ChartTooltipContent
+                  className="w-[150px]"
+                  nameKey="views"
+                  labelFormatter={(value) => {
+                    return new Date(value).toLocaleDateString("pt-BR", {
+                      month: "short",
+                      day: "numeric",
+                      year: "numeric",
+                    });
+                  }}
+                />
+              }
+            />
+            <Bar dataKey={"time"} fill={"hsl(var(--chart-2))"} />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="h-fit max-h-10">
+        <div className="flex w-full items-start gap-2 text-sm">
+          <div className="grid gap-2">
+            <div className="flex items-center gap-2 font-medium leading-none">
+              Historico recente <TrendingUp className="h-4 w-4" />
+            </div>
+          </div>
+        </div>
+      </CardFooter>
+    </Card>
   );
 }
 
